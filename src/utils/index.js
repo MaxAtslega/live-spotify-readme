ff0c1bfd91eedd13e422f1d920708fb8ba6dc476
Max Atslega <max@atslega.de>
2021-10-03 18:46:08 +0200

    fix: Docker and code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,7 @@
 import fillMixedTextLeft from "./fillMixedTextLeft";
-import fillMixedTextRight from "./fillMixedTextRight";
 import roundRect from "./roundRect";
 
 module.exports = {
   fillMixedTextLeft,
-  fillMixedTextRight,
   roundRect
 }
